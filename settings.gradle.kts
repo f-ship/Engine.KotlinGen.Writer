c8012cb666706965b7bcf598e081b1437d9bec3b
Sam Taiwo <staiwo@onlydates.fan>
2024-12-24 21:38:42 +0000

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+rootProject.name = "writer"
+rootProject.buildFileName = "engine.kotlingen.writer.gradle.kts"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
